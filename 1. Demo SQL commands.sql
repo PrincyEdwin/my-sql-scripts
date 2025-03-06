a6120109db351adda43521675277d391a859141f
PrincyEdwin <rmahibaprincy@gmail.com>
2025-03-06 11:06:15 -0600

    Initial commit: Add SQL scripts

---
+++ 1. Demo SQL commands.sql
@@ -0,0 +1,11 @@
+SELECT * FROM parks_and_recreation.employee_demographics;
+
+SELECT * FROM parks_and_recreation.employee_salary;
+
+SELECT first_name,
+age,
+(age + 10) * 10 
+FROM parks_and_recreation.employee_demographics;
+
+
+
